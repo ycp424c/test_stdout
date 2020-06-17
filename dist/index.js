8f6a432a199a055207e495afdf6ea048d63c1191
justynchen <justynchen@tencent.com>
2020-06-17 11:46:39 +0800

    feat:init

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const path = require("path");
+function wrapLog() {
+    const logFilePath = path.resolve(__dirname, '../log/log.txt');
+    if (!fs.existsSync(logFilePath)) {
+        fs.writeFileSync(logFilePath, '');
+    }
+    let logFileStream = fs.createWriteStream(logFilePath, { flags: 'a' });
+    process.stdout.write = process.stderr.write = logFileStream.write.bind(logFileStream);
+}
+wrapLog();
+console.log('you mother fucker');
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+function wrapLog(){
+	const logFilePath = path.resolve(__dirname,'../log/log.txt')
+
+	if(!fs.existsSync(logFilePath)){
+		fs.writeFileSync(logFilePath,'')
+	}
+
+	let logFileStream = fs.createWriteStream(logFilePath,{flags:'a'})
+
+	process.stdout.write = process.stderr.write = logFileStream.write.bind(logFileStream);
+
+}
+
+wrapLog()
+
+console.log('little juli')
